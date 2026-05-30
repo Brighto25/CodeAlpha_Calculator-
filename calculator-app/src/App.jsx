362ba04a4a5be04c727a3012662097222231dc8a
Brighto25 <Bright.Addaneyf3@gmail.com>
2026-05-30 16:24:46 +0000

    change button order

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -11,22 +11,22 @@ function App() {
 
   const buttons = [
     "A/C",
+    "÷",
+    "x",
+    "-",
     "7",
     "8",
     "9",
-    "÷",
+    "+",
     "4",
     "5",
     "6",
-    "x",
+    "=",
     "1",
     "2",
     "3",
-    "-",
     "0",
     ".",
-    "=",
-    "+",
   ];
   function handleClick(label) {
     if (label === "A/C") {
